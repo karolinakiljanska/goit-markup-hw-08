a7361cb7584133caa08170add7c4b114e6cf4d4e
karolinakiljanska <karolinakiljanska112@gmail.com>
2022-12-03 00:12:00 +0100

    poprawki

--- js.modal/menu.js
+++ js.modal/menu.js
@@ -3,13 +3,11 @@
   const menuCloseBtn = document.querySelector("[data-menu-close]");
 
   const mobileMenu = document.querySelector("[data-menu]");
-  // const body = document.querySelector('body');
 
   menuOpenBtn.addEventListener("click", openModal);
   menuCloseBtn.addEventListener("click", openModal);
 
   function openModal() {
     mobileMenu.classList.toggle("is-open");
-    // body.classList.toggle('no-scroll');
   }
 })();
